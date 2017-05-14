061ab8750a4b629577c3990694c00eae50634617
Aleksandar Krstić <acckrs@gmail.com>
2017-05-14 23:16:36 +0200

    e

--- powershell/get-osinfo1.ps1
+++ powershell/get-osinfo1.ps1
@@ -2,17 +2,32 @@ param (
     [string[]]$computerName
 )
 foreach ($computer in $computerName){
-    $session = New-CimSession -ComputerName $computer 
+   try {
+    $session = New-CimSession -ComputerName $computer  -ErrorAction Stop
     $os= Get-CimInstance -CimSession $session -ClassName win32_operatingsystem
     $cs= Get-CimInstance -CimSession $session -ClassName win32_computersystem   
 
     $properties=@{computerName=$computer
+                  status='Connected'
                   SPVersion=$os.ServicePackMinorVersion # .ServicePackMajorVersion
                   OSVersion=$os.version
                   Model=$cs.Model
-                  freeRAM=$cs.TotalPhysicalMemory/1GB
+                  RAM=$cs.TotalPhysicalMemory/1GB
                   Mfgr=$cs.Manufacturer}
 
-    $obj = New-Object -TypeName psobject -Property $properties 
-    Write-output $obj
+    }
+    catch{
+
+    $properties=@{computerName=$computer
+                  status='Disconnected'
+                  SPVersion=$null
+                  OSVersion=$null
+                  Model=$null
+                  RAM=$null
+                  Mfgr=$null}
+        }
+        finally {
+            $obj = New-Object -TypeName psobject -Property $properties 
+            Write-output $obj
+        }
 }
