feb9f12a35c51d2cefd045e50c7db4b7dc5d2865
Aleksandar Krstić <acckrs@gmail.com>
2017-05-27 14:04:42 +0200

    ps toolmaking lab answers addedd

--- powershell/books/ps toolmaking scripts/ch7/get-sysInfo2.ps1
+++ powershell/books/ps toolmaking scripts/ch7/get-sysInfo2.ps1
@@ -1,8 +1,30 @@
-function get-sysInfo {
+function get-sysInfo2 {
     [cmdletbinding()]
     param(
-        [Parameter(mandatory=$true, 
-                 ValueFromPipeline = $true)]
-        [string[]] $ComputerName
+        [parameter(Mandatory = $true,
+            ValueFromPipeline = $true)]
+        [string[]] $computerName
     )
+
+    BEGIN {}
+
+    PROCESS {
+        foreach ($computer in $ComputerName) {
+            $os = Get-WmiObject -Class win32_operatingSystem -ComputerName $Computer
+            $comp = Get-WmiObject -Class win32_computerSystem -ComputerName $Computer
+
+            $props=@{"ComputerName"=$computer;
+                        "Model"=$comp.Model;
+                        "Manufacturer"=$comp.Manufacturer;
+                        "OSVersion"=$os.Version;
+                        "LastBootTime"=$os.convertToDateTime($os.lastBootUpTime);
+                        "InstallDate"=$os.convertToDateTime($os.InstallDate)}
+            
+            $obj=New-Object -TypeName psobject -Property $props
+            Write-Output $obj
+        }
+    }
+
+    END {}
 }
+get-sysInfo2 -computerName localhost
