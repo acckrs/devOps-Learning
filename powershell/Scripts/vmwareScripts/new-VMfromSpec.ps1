06ff4371df00bd83dceae354070b2490dd0feec9
Admin Aleksandar Krstic <ADMIN_KALEKSANDAR@deltabank.co.yu>
2017-06-01 15:42:36 +0200

    d

--- powershell/Scripts/vmwareScripts/new-VMfromSpec.ps1
+++ powershell/Scripts/vmwareScripts/new-VMfromSpec.ps1
@@ -8,30 +8,39 @@ function new-VMfromSpec {
         [string] $ip,
 
         
-        [string] $vcenter="bib-vcentar-01",
+        [string] $vcenter = "bib-vcentar-01",
 
         [Parameter(Mandatory = $true)]
-        [validateSet("SECURITY","BIB-LOCAL","BIB-DMZ")]
+        [validateSet("SECURITY", "BIB-LOCAL", "BIB-DMZ")]
         [string] $cluster
         
     )
 
     BEGIN {
         Connect-VIServer -Server $vcenter
-        $template = Get-Template -Name "Win2012R2Template"
+        $usedDS = (Get-Datastore | sort FreeSpaceGB -Descending | select -ExpandProperty name -first 1 | Out-String)
+        $template = Get-Template -Name "blank_w2k12r2"
         $vmhost = Get-Cluster $cluster | Get-VMHost -State Connected | Get-Random
-        $spec=Get-OSCustomizationSpec -Name  "CustomOsSpec1" | New-OSCustomizationSpec  -Name "tmp01" -Type NonPersistent | Set-OSCustomizationSpec -Name "tmp01"
 
-    }
+        if (Get-OSCustomizationspec -name tmp01) {
+            Remove-OSCustomizationSpec -OSCustomizationSpec tmp01
+            $spec = Get-OSCustomizationSpec -Name  "w2k12r2_spec1" | New-OSCustomizationSpec  -Name "tmp01" -Type NonPersistent | Set-OSCustomizationSpec -Name "tmp01"
+        }
+        else {
+            $spec = Get-OSCustomizationSpec -Name  "w2k12r2_spec1" | New-OSCustomizationSpec  -Name "tmp01" -Type NonPersistent | Set-OSCustomizationSpec -Name "tmp01"
+        }
+    } #end BEGIN
 
     PROCESS {
 
         Get-OSCustomizationNicMapping -Spec $spec.name| Set-OSCustomizationNicMapping -IpMode UseStaticIP -IpAddress $ip -SubnetMask '255.255.255.0' -Dns '10.35.170.22' -DefaultGateway '10.35.221.1' 
+        Write-Verbose "Creating vm $vmname on host $vmhost with IP address $ip from $template template. Vm will be on $usedDS datastore"
         New-VM -Name $vmName `
             -VMHost $vmhost `
             -Template $template `
-            -Datastore DS3 `
+            -Datastore $usedDS `
             -OSCustomizationSpec $spec 
+        Start-VM -VM $vmName
     }
 
     END {}
@@ -39,4 +48,4 @@ function new-VMfromSpec {
 
 }
 
-new-VMfromSpec -vmName "se-bg-safeguard" -ip "10.35.221.194" -cluster "SECURITY"
+new-VMfromSpec -vmName "se-bg-safeguard" -ip "10.35.221.194" -cluster "SECURITY" -verbose
