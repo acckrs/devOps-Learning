01f4ef3b1f419f9c9a180f7a1f807938452e64d5
Aleksandar Krstić <acckrs@gmail.com>
2018-02-24 12:51:33 +0100

    bravo za mene

--- powershell/Scripts/Storage/map-diskinfo.ps1
+++ powershell/Scripts/Storage/map-diskinfo.ps1
@@ -1,27 +1,28 @@
 $3par_full_str = gc .\3par.txt
 $3par_props = $3par_full_str|select -Skip 1 -First 1
-$3parvalues = $3par_full_str|select -Skip 3 
+$3parvalues_first_row = $3par_full_str|select -Skip 3 -first 1
+$3parvalues_first_row_array = $3parvalues_first_row -split '\s+'
 
-$3pprops_array = $3par_props -split '\s+' 
-$3pprops.Count
-$all = @()
-$rowCount = 0
-foreach ($3parvalue_row in $3parvalues) {
-    $rowCount++
-    write-host "Broj reda je ",$rowCount
-    $obj = New-Object -TypeName PSObject
-    $3parvalue_row_array = $3parvalue_row -split '\s+'
-    $rowValueCount = 0
-    foreach ($3parvalue_row_array_member in $3parvalue_row_array) {
-        $rowValueCount++
-        write-host "Broj vrednosti reda je ",$rowValueCount
-        $rowValueCountInc = 0
-        foreach ($3pprops_array_member in $3pprops_array) {
-            $rowValueCountInc++
-            write-host "Broj vrednosti Inkrementa reda je ",$rowValueCountInc
-            #    Add-Member -InputObject $obj -MemberType NoteProperty -Name $3pprops_array_member -Value $3parvalue_row_array_member  -PassThru -Force
+$3parvalues_rows = $3par_full_str|select -Skip 3 
+$all=@()
+foreach($3parvalues_row in $3parvalues_rows){
+    $3parvalues_row_array=$3parvalues_row -split '\s+' 
+    $props_hash = @{}
+    for ($i = 0; $i -lt $3parvalues_row_array.Count; $i++) {
+        $prop_name = "Prop " + "$i"
+       
+        $props = @{
+            "$prop_name" = $3parvalues_row_array[$i];
         }
+        $props_hash += $props
     }
-        
+    
+    $obj = New-Object -TypeName PSObject -Property $props_hash
+    $all+=$obj
+    
 }
+$all
+
+
+
 
