f59102c8929f21a1f02fa6ff2727255a06d9377d
Aleksandar Krstić <acckrs@gmail.com>
2018-09-16 21:19:59 +0200

    y

---
+++ Azure/mg-start/2m2/deploy-linux-vm-cli.ps1
@@ -0,0 +1,21 @@
+﻿break
+# #############################################################################
+# Deploy Linux VMs to Azure with Azure CLI v2.0
+# Repo: https://github.com/timothywarner/linux
+# AUTHOR:  Tim Warner
+# EMAIL: timothy-warner@pluralsight.com
+# TWITTER: @TechTrainerTim
+# WEB: timw.info
+# #############################################################################
+
+ssh-keygen -t rsa -b 2048 -C "acckrs@vm-azuretraining-acckrslin" -f c:/users/acckr/.ssh/id_rsa -N Animacija132!
+
+az
+
+az login
+
+#az group create -n myResourceGroup -l eastus
+
+az vm create --image credativ:Debian:8:latest --admin-username acckrs --ssh-key-value C:/users/acckr/.ssh/id_rsa.pub --public-ip-address-dns-name vm-azuretraining-acckrslin --resource-group azuretraining --location centralus --name vm-azuretraining-acckrslin
+
+ssh acckrs@vm-azuretraining-acckrslin.centralus.cloudapp.azure.com
