4b59dbb2089bd7d4f6adcc111d286539cd283aed
Aleksandar Krstić <acckrs@gmail.com>
2018-09-30 17:35:06 +0200

    i

---
+++ Azure/arm-master/armMastering/radno.ps1
@@ -0,0 +1,1 @@
+New-AzureRmResourceGroupDeployment -Name <deployment-name> -ResourceGroupName <resource-group-name> -TemplateUri https://raw.githubusercontent.com/azure/azure-quickstart-templates/master/201-vm-domain-join-existing/azuredeploy.json
